7fa670b9a7775420a3e1c508ef3eb4bfe639f5f3
Andro87 <ggandolfo987@gmail.com>
2020-11-25 18:02:00 +0100

    [Initial] -Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,281 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Box from '../components/box'
+import React, {useState} from "react" 
+import { useEmblaCarousel } from 'embla-carousel/react'
+
 
 export default function Home() {
+
+  const [open,setOpen] = useState(false)
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Frontend Mentor | Room homepage</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.viewportCss} ref={emblaRef}>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={styles.containerCss}>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className={styles.slideCss}> 
+            <div className={styles.top}>  
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+              <div className={styles.navigation}>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                <div className={`${styles.menu} ${open?styles.menumobile:null}`}>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                  <button className={styles.hamburger}
+                  onClick={()=>setOpen(!open)}
+                  >
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+                  {!open?(  
+                    <img src="./images/icon-hamburger.svg" alt="icon" />
+                  
+                  ): <img src="./images/icon-close.svg" alt="icon" />}
+
+                  </button>
+
+                  {!open?(
+                    <img src="/images/logo.svg"  alt="logo"  className={styles.logo}/>
+                  ):null}    
+
+
+                  {open?( 
+                    <div className={styles.linkmobile}>
+
+                      <a href="*"> home</a>
+                      <a href="*"> shop</a>
+                      <a href="*"> about</a>
+                      <a href="*"> contact</a>
+
+                    </div>
+
+                  ):null}  
+
+                  <div className={styles.link}>
+
+                    <a href="*"> home</a>
+                    <a href="*"> shop</a>
+                    <a href="*"> about</a>
+                    <a href="*"> contact</a>
+
+                  </div>
+
+                </div>  
+
+              </div>
+
+              <Box padding="top">
+
+                <h1>Discover innovative ways to decorate</h1>
+                <p>We provide unmatched quality, comfort, and style for property owners across the country. 
+                  Our experts combine form and function in bringing your vision to life. Create a room in your 
+                  own style with our collection and make your property a reflection of you and what you love.
+                </p>
+
+                <p className={styles.shop} >SHOP NOW  <span> <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg></span> </p>  
+        
+              </Box>
+
+            </div> 
+
+          </div>
+
+         
+
+
+
+             
+
+            
+
+          <div className={styles.slideCss}> 
+            <div className={styles.top}>  
+
+              <div className={styles.navigation}>
+
+                <div className={`${styles.menu} ${open?styles.menumobile:null}`}>
+
+                  <button className={styles.hamburger}
+                  onClick={()=>setOpen(!open)}
+                  >
+
+                  {!open?(  
+                    <img src="./images/icon-hamburger.svg" alt="icon" />
+                  
+                  ): <img src="./images/icon-close.svg" alt="icon" />}
+
+                  </button>
+
+                  {!open?(
+                    <img src="/images/logo.svg"  alt="logo"  className={styles.logo}/>
+                  ):null}    
+
+
+                  {open?( 
+                    <div className={styles.linkmobile}>
+
+                      <a href="*"> home</a>
+                      <a href="*"> shop</a>
+                      <a href="*"> about</a>
+                      <a href="*"> contact</a>
+
+                    </div>
+
+                  ):null}  
+
+                  <div className={styles.link}>
+
+                    <a href="*"> home</a>
+                    <a href="*"> shop</a>
+                    <a href="*"> about</a>
+                    <a href="*"> contact</a>
+
+                  </div>
+
+                </div>  
+
+              </div>
+
+              <Box padding="top">
+
+                <h1> We are available all across the globe </h1>
+                <p> With stores all over the world, it's easy for you to find furniture for your home or place of business. 
+                  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
+                  store locator. Any questions? Don't hesitate to contact us today.
+                </p>
+
+                <p className={styles.shop} >SHOP NOW  <span> <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg></span> </p>  
+        
+              </Box>
+
+            </div> 
+
+          </div>
+
+          <div className={styles.slideCss}>
+
+
+            <div className={styles.top}>  
+
+              <div className={styles.navigation}>
+
+                <div className={`${styles.menu} ${open?styles.menumobile:null}`}>
+
+                  <button className={styles.hamburger}
+                  onClick={()=>setOpen(!open)}
+                  >
+
+                  {!open?(  
+                    <img src="./images/icon-hamburger.svg" alt="icon" />
+                  
+                  ): <img src="./images/icon-close.svg" alt="icon" />}
+
+                  </button>
+
+                  {!open?(
+                    <img src="/images/logo.svg"  alt="logo"  className={styles.logo}/>
+                  ):null}    
+
+
+                  {open?( 
+                    <div className={styles.linkmobile}>
+
+                      <a href="*"> home</a>
+                      <a href="*"> shop</a>
+                      <a href="*"> about</a>
+                      <a href="*"> contact</a>
+
+                    </div>
+
+                  ):null}  
+
+                  <div className={styles.link}>
+
+                    <a href="*"> home</a>
+                    <a href="*"> shop</a>
+                    <a href="*"> about</a>
+                    <a href="*"> contact</a>
+
+                  </div>
+
+                </div>  
+
+              </div>
+
+              <Box padding="top">
+
+                <h1> Manufactured with the best materials</h1>
+                <p> Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
+                  to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+                  experience in this industry, we understand what customers want for their home and office.
+                </p>
+
+                <p className={styles.shop} >SHOP NOW  <span> <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg></span> </p>  
+        
+              </Box>
+
+            </div> 
+
+          </div>  
+                  
+        </div>  
+
+      </div> 
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <button className={styles.switch}>
+
+        <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg>
+        <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg>
+
+      </button>
+
+      
+
+      <div className={styles.bottom}>
+
+        <div className={styles.column1}></div>
+
+        <Box padding="bottom">
+
+          <h4>ABOUT OUR FURNITURE</h4>
+          <p> Our multifunctional collection blends design and function to suit your individual taste.
+          Make each room unique, or pick a cohesive theme that best express your interests and what
+          inspires you. Find the furniture pieces you need, from traditional to contemporary styles
+          or anything in between. Product specialists are available to help you create your dream space.</p>
+
+        </Box>
+
+        <div className={styles.column3}></div>
+
+        
+
+      </div>
+
+      
     </div>
   )
 }
